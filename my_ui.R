7ebff94a560d2db1f93670cf0ed7f8f79cd9f2d5
Samyak Jain <iamsamyakjain@gmail.com>
2020-06-21 09:29:18 +0530

    Update question 3

--- my_ui.R
+++ my_ui.R
@@ -153,6 +153,38 @@ Question2 <- tabPanel(
   )
 )
 
+Question3 <- tabPanel(
+  "Bitcoin vs Ethereum Volume",
+  fluidRow(
+    h1("How do the trading volumes of Bitcoin and Ethereum compare with each other over time?"),
+    p("Volume represents, among other things, potential and interest in the currency. 
+         For example, if we see that both currencies have increasing volume over time, we may believe that cryptocurrency may be getting more popular, and that we may see even more investment in the near future. 
+         Our datasets for Bitcoin and Ethereum have 2 features that measure volume, 1 for USD and 1 in terms of their own currency (BTC/ETH). The USD feature was used as it would make more sense to an audience that likely has more knowledge of USD than knowledge of cryptocurrency
+        "),
+    br()
+  ),
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput(inputId = "year3", p("Select year"),value = 2017, min = 2017, max = 2020, step = 1),
+      br(),
+      checkboxGroupInput("coinType3",
+                         p("Select the cryptocurrency you would like to include:"),
+                         choices = c("Bitcoin", "Ethereum")),
+    ),
+    mainPanel(
+      fluidRow(
+        plotOutput("volumeAnalysis"),
+      ),
+      h3("Results and Implications"),
+      p("Based on the statistics, it was clear that bitcoin was the favored competitor in terms of cryptocurrencies trading. 
+      But what's suprising was that Ethereum's volume trading wasn't as small as we thought it would. 
+      We were assuming that Bitcoin would be the monopoly in the cryptocurrency business because in a surface level, users tends to only know about Bitcoin. 
+      Because of the volume, it definitely clarify our assumptions of Bitcoin being the only competitor. 
+      Thus, the results demonstrates that Bitcoin is popular cryptocurrencies and it doesn't mean they're the only dominating the field. ")
+    )
+  )
+)
+
 my_ui <- shinyUI(
   fluidPage(
     theme = shinytheme("cerulean"),
