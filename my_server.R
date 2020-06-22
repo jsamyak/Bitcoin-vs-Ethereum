2111db78db31bad035e151e672fda578fcfcb3d9
Samyak Jain <iamsamyakjain@gmail.com>
2020-06-23 01:35:58 +0530

    Update server - raw data input

--- my_server.R
+++ my_server.R
@@ -19,5 +19,24 @@ server <- function(input, output) {
     coinTypeTemp2 <- paste(input$coinType3, collapse = ", ")
     volume(as.character(input$year3), coinTypeTemp2)
   })
+  
+  output$RawDataBitcoin <- DT::renderDataTable(
+    DT::datatable({
+      bitcoin
+    },
+    options = list(lengthMenu=list(c(5,15,20),c('5','15','20')),pageLength=10,
+                   initComplete = JS(
+                     "function(settings, json) {",
+                     "$(this.api().table().header()).css({'background-color': 'moccasin', 'color': '1c1b1b'});",
+                     "}"),
+                   columnDefs=list(list(className='dt-center',targets="_all"))
+    ),
+    filter = "top",
+    selection = 'multiple',
+    style = 'bootstrap',
+    class = 'cell-border stripe',
+    rownames = FALSE,
+    colnames = c("Date","Symbol","Open","High","Low","Close","Volume BTC","Volume USDT")
+    ))
 }
 
